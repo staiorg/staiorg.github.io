58ef4eeb9838dbdb58a7be22bcd85bc06cd3251d
Reedit Shahriar <reeditsyed@gmail.com>
2018-07-28 11:43:33 -0700

    make videos display work

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 // TODO: run from Apache server to avoid error
 //$.getScript("tplawesome.js");
 
-var loaded = false;
-
 $(function(){
 	// default is date
 	var order = "date";
@@ -92,10 +90,62 @@ function init(){
 	});
 	
 	// show all videos
-	/*if (!loaded){
-		$(function(){document.forms["myform"].submit();});
-		loaded = true;
-	}*/
+	$(function(){
+		e.preventDefault();
+		console.log("Submitting form...");
+		
+		// build YouTube Search API request
+		var request = gapi.client.youtube.search.list({
+			
+			// specify that we are looking for video snippets
+			part: "snippet",
+			type: "video",
+			
+			// replace spaces with pluses for GET request
+			q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+			
+			// number and order of videos received
+			// most recent -> "date", most popular -> "viewCount"
+			maxResults: 5,
+			order: order,
+			
+			// specify that we only want videos from our channel
+			channelId: "UCdXtMm5nSlpZez3nkciKpng"
+			
+		});
+		
+		// execute HTTP request
+		request.execute(function(response){
+			
+			console.log("Executed search request.");
+			
+			// collect results
+			var results = response.result;
+			
+			// empty video display div
+			$("#videosbody").html("");
+			
+			console.log("Number of search results: "+results.items.length);
+			
+			// if there are no applicable results, tell user
+			if (results.items.length == 0){
+				
+				console.log("No search results found.");
+				$("#videosbody").html('No tutorials found. Try entering something else.');
+				
+			} 
+			// otherwise, show the results to user
+			else {
+				$.each(results.items, function(index, item) {
+					$.get("item.html", function(data) {
+						$("#videosbody").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+					});
+				});
+				resetVideoHeight();
+			}
+		});
+	});
+	
 }
 
 // video formatting
