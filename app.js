1d83ceae3aa154d85e1f20ee9ab513eb23766458
Sam Yang <samuel.q.yang@gmail.com>
2018-07-29 20:35:31 -0700

    youtube vid class

--- app.js
+++ app.js
@@ -5,81 +5,10 @@
 $(function(){
 	// default is date
 	var order = "date";
-	console.log("Document ready");
+	console.log("Document ready.");
 	
 	$("form").on("submit",function(e){
 		e.preventDefault();
-		
-		// build YouTube Search API request
-		var request = gapi.client.youtube.search.list({
-			
-			// specify that we are looking for video snippets
-			part: "snippet",
-			type: "video",
-			
-			// replace spaces with pluses
-			q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-			
-			// number and order of videos received
-			// most recent -> "date", most popular -> "viewCount"
-			maxResults: 5,
-			order: order,
-			
-			// specify that we only want videos from our channel
-			channelId: "UCdXtMm5nSlpZez3nkciKpng"
-		});
-		
-		request.execute(function(response){
-			console.log("Executed search request!");
-			
-			var results = response.result;
-			$("#videosbody").html("");
-			$.each(results.items, function(index, item) {
-				$.get("item.html", function(data) {
-					$("#videosbody").append("<div class=\"youtubevid\">" + tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]) + "</div>");
-			
-			console.log("Number of search results: "+results.items.length);
-			
-			// if there are no applicable results, tell user
-			if (results.items.length == 0){
-				
-				console.log("No search results found.");
-				$("#videosbody").html('No tutorials found. Try entering something else.');
-				
-			} 
-			// otherwise, show the results to user
-			else {
-				$.each(results.items, function(index, item) {
-					$.get("item.html", function(data) {
-						$("#videosbody").append("<div class=\"youtubevid\">" + tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]) + "</div>");
-					});
-				});
-			});
-			resetVideoHeight();
-		});
-	});
-});
-
-$('#popbutt').on('click', function(){
-	order = "viewCount";
-});
-
-$('#recbutt').on('click', function(){
-	order = "date";
-});
-
-function init(){
-	
-	// TODO hide API key
-	gapi.client.setApiKey("AIzaSyA38kJ-p-UZLgjf3QWqKbABsgLFiAqbXfg");
-	gapi.client.load("youtube","v3",function(){
-		// yt api is ready
-		console.log("YouTube Search API is ready");
-	});
-	
-	// show all videos
-	$(function(){
-		//e.preventDefault();
 		console.log("Submitting form...");
 		
 		// build YouTube Search API request
@@ -134,8 +63,92 @@ function init(){
 		});
 	});
 	
+	$(window).on("resize", resetVideoHeight);
+});
+
+// when the filter buttons are clicked, set filters
+$('#popbutt').on('click', function(){
+	order = "viewCount";
+	console.log(order);
+});
+
+$('#recbutt').on('click', function(){
+	order = "date";
+	console.log(order);
+});
+
+// function to initialize gAPI client
+function init(){
+	
+	console.log("Initializing Google API client.");
+	
+	// TODO hide API key
+	gapi.client.setApiKey("AIzaSyA38kJ-p-UZLgjf3QWqKbABsgLFiAqbXfg");
+	gapi.client.load("youtube","v3",function(){
+		// yt api is ready
+		//console.log("YouTube Search API is ready");
+	});
+	
+	// show all videos
+	$(function(){
+		//e.preventDefault();
+		console.log("Submitting form...");
+		
+		// build YouTube Search API request
+		var request = gapi.client.youtube.search.list({
+			
+			// specify that we are looking for video snippets
+			part: "snippet",
+			type: "video",
+			
+			// replace spaces with pluses for GET request
+			q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+			
+			// number and order of videos received
+			// most recent -> "date", most popular -> "viewCount"
+			maxResults: 5,
+			order: order,
+			
+			// specify that we only want videos from our channel
+			channelId: "UCdXtMm5nSlpZez3nkciKpng"
+			
+		});
+		
+		// execute HTTP request
+		request.execute(function(response){
+			
+			console.log("Executed search request.");
+			
+			// collect results
+			var results = response.result;
+			
+			// empty video display div
+			$("#videosbody").html("");
+			
+			console.log("Number of search results: "+results.items.length);
+			
+			// if there are no applicable results, tell user
+			if (results.items.length == 0){
+				
+				console.log("No search results found.");
+				$("#videosbody").html('No tutorials found. Try entering something else.');
+				
+			} 
+			// otherwise, show the results to user
+			else {
+				$.each(results.items, function(index, item) {
+					$.get("item.html", function(data) {
+						$("#videosbody").append("<div class=\"youtubevid\">" + tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]) + "</div>");
+					});
+				});
+				resetVideoHeight();
+			}
+		});
+	});
+	
 }
 
+// video formatting
 function resetVideoHeight() {
 	$(".video").css("height", $("#videosbody").width() * 9/16);
 }
@@ -151,6 +164,7 @@ function tplawesome(template, data) {
 	}
 	return res;
 }
+
 
 
 
